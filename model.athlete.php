2842f436f402aac9264a7ca2c0f8008977a94152
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-19 13:15:58 -0500

    Create model.athlete.php

---
+++ model.athlete.php
@@ -0,0 +1,15 @@
+<?php
+function selectAthlete() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Athlete_ID, Athlete_Name, Athlete_Age FROM `Sports Athlete`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
