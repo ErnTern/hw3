dcbcb67ce8cde134db285d5d67ebc8a7d9efcf2d
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-19 12:58:15 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -20,16 +20,16 @@
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="post.php">Post</a>
+          <a class="nav-link" href="athlete.php">Athlete</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="post-result.php">Post Result</a>
+          <a class="nav-link" href="sport.php">Sport</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="get.php">Get</a>
+          <a class="nav-link" href="athlete-sport.php">Athlete's Sport</a>
         </li>
          <li class="nav-item">
-          <a class="nav-link" href="get-result.php">Get Result</a>
+          <a class="nav-link" href="athlete-league.php">Athlete's League</a>
         </li>
       </ul>
     </div>
