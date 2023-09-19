3d0c916b759ad91d0000f7b9e6e9478d254d064a
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-19 15:44:56 -0500

    Update view-athlete.php

--- view-athlete.php
+++ view-athlete.php
@@ -1,20 +1,25 @@
 <h1>Athletes</h1>
 <div class = "table-response">
-<table class = "table">
-<thread>
-  <tr>
-    <th>ID</th>
-  <th>Name</th>
-  <th>Age</th>
-  </tr>
-</thread>
-<tbody>
+  <table class = "table">
+    <thread>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Age</th>
+      </tr>
+    </thread>
+  <tbody>
   <?php
 while ($athlete = $athlete->fetch_assoc())
-  ?>
-<tr>
-  <td><?php echo $athlete['athlete_id']; ?></td>
-  <td><?php echo $athlete['athlete_name']; ?></td>
-  <td><?php echo $athlete['athlete_age']; ?></td>
-</tr>
-</tbody>
+?>
+  <tr>
+    <td><?php echo $athlete['athlete_id']; ?></td>
+    <td><?php echo $athlete['athlete_name']; ?></td>
+    <td><?php echo $athlete['athlete_age']; ?></td>
+  </tr>
+<?php
+}
+?>
+    </tbody>
+  </table>
+</div>
