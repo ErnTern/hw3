dfe22acdcec67e07569f7da520153d69492b90c8
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-20 16:18:52 -0500

    Create sport-by-athlete.php

---
+++ sport-by-athlete.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-sport.php");
+
+$pgaetitle = "Sports";
+include "view-header.php";
+$sports = selectSport();
+include "view-sport.php";
+include "view-footer.php";
+?>
