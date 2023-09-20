a8edf4a4f47499d8049c7e928620751ea2c9f9c8
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-20 16:21:22 -0500

    Update sport-by-athlete.php

--- sport-by-athlete.php
+++ sport-by-athlete.php
@@ -1,10 +1,10 @@
 <?php
 require_once("util-db.php");
-require_once("model-sport.php");
+require_once("model.sport-by-athlete.php");
 
-$pgaetitle = "Sports";
+$pgaetitle = "Sports played by Athletes";
 include "view-header.php";
-$sports = selectSport();
-include "view-sport.php";
+$sports = selectSportByAthlete($_GET['id']);
+include "view-sport-by-athlete.php";
 include "view-footer.php";
 ?>
