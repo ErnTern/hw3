486793dd36d5abca011686d1d066e0dc6ceb8ecd
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-25 13:36:45 -0500

    Update model.sport-by-athlete.php

--- model.sport-by-athlete.php
+++ model.sport-by-athlete.php
@@ -2,7 +2,7 @@
 function selectSportByAthlete($aid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare(SELECT s.sport_id, sport_name, average_playtime, league_name, team, number_of_players FROM Sport s JOIN League l on l.sport_id = s.sport_id WHERE l.athlete_id = ?);
+        $stmt = $conn->prepare("SELECT s.sport_id, sport_name, average_playtime, league_name, team, number_of_players FROM Sport s JOIN League l on l.sport_id = s.sport_id WHERE l.athlete_id = ?");
       $stmt->bind_param("a", $aid);  
       $stmt->execute();
         $result = $stmt->get_result();
