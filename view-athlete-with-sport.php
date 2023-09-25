8f49dc2352b20ef753adced19a68d7ae1648d9d3
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-09-25 14:28:56 -0500

    Update view-athlete-with-sport.php

--- view-athlete-with-sport.php
+++ view-athlete-with-sport.php
@@ -8,14 +8,18 @@ while ($athlete = $athletes->fetch_assoc())  {
     <div class="card-body">
       <h5 class="card-title"><?php echo $athlete['athlete_name']; ?></h5>
       <p class="card-text">
+       <ul class="list-group">
   <?php
     $sports = selectSportByAthlete($athlete['athlete_id']);
   while ($sport = $sports->fetch_assoc())  {
     ?>
+     <li class="list-group-item"><?php echo $sport['sport_name']; ?> - <?php echo $sport['average_playtime']; ?> - <?php echo $sport['league_name']; ?> - <?php echo $sport['team']; ?> - <?php echo $sport['number_of_players']; ?></li>
+        
     <?php
   }
   ?>
-      </p>
+       </ul>
+       </p>
       <p class="card-text"><small class="text-body-secondary">Age: <?php echo $athlete['athlete_age']; ?></small></p>
     </div>
   </div>
