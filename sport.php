f594aca7805d708563fb88ed30ddf12ab048676a
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-06 18:56:06 -0500

    Update sport.php

--- sport.php
+++ sport.php
@@ -14,6 +14,13 @@ if (isset($_POST['actionType'])) {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
+    case "Edit":
+      if (updateSport($_POST['sName'], $_POST['sPlaytime'], $_POST['sid')) {
+        echo '<div class="alert alert-success" role="alert">Sport edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
      case "Delete":
       if (deleteSport($_POST['sid'])) {
         echo '<div class="alert alert-success" role="alert">Sport deleted.</div>';
