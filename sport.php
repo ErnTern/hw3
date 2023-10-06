3e360ae0551c06b26b7d946ba66f5d0027ec2a11
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-06 18:05:57 -0500

    Update sport.php

--- sport.php
+++ sport.php
@@ -4,6 +4,15 @@ require_once("model.sport.php");
 
 $pgaetitle = "Sports";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      insertSport($_POST['sName'], $_POST['sPlaytime']);
+      break;
+  }
+}
+
 $sports = selectSport();
 include "view-sport.php";
 include "view-footer.php";
