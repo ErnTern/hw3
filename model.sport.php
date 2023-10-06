e77569978b18b4952868a8fd991ff75e55a98cc8
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-06 17:34:04 -0500

    Update model.sport.php

--- model.sport.php
+++ model.sport.php
@@ -12,4 +12,46 @@ function selectSport() {
         throw $e;
     }
 }
+
+function insertSport($sName, $sPlaytime) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Sport` (`sport_name`, `average_playtime`) VALUES (?, ?");
+        $stmt->bind_param("ss", $sName, $sPlaytime);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateSport($sName, $sPlaytime, $sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Sport` set `sport_name` = ?, `average_playtime` = ? where sport_id = ?");
+        $stmt->bind_param("ssi", $sName, $sPlaytime, $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteSport($sid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from sport where sport_id = ?");
+        $stmt->bind_param("i", $sid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
