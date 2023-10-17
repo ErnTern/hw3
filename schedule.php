5e85b485a93d2fde918ef7466ef15b230cdb4641
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-17 11:16:44 -0500

    Create schedule.php

---
+++ schedule.php
@@ -0,0 +1,37 @@
+<?php
+require_once("util-db.php");
+require_once("model.schedule.php");
+
+$pgaetitle = "Schedules";
+include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertSchedule($_POST['schName'], $_POST['schID'])) {
+        echo '<div class="alert alert-success" role="alert">New Season/Tourney Added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateSchedule($_POST['schName'], $_POST['schID'], $_POST['sid'])) {
+        echo '<div class="alert alert-success" role="alert">Season/Tourney edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+     case "Delete":
+      if (deleteSchedule($_POST['sid'])) {
+        echo '<div class="alert alert-success" role="alert">Season/Tourney deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$schedule = selectSchedule();
+include "view-schedule.php";
+include "view-footer.php";
+?>
