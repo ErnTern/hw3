d7199120108c3024505e051deb3161323d137ed7
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-23 16:04:43 -0500

    Update league-schedule.php

--- league-schedule.php
+++ league-schedule.php
@@ -4,6 +4,19 @@ require_once("model.schedule.php");
 
 $pgaetitle = "Schedule";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertSchedule($_POST['schName'], $_POST['schID'], $_POST['schSDate'], $_POST['schEDate'])) {
+        echo "<p>success</p>";
+      } else {
+        echo "<p>error</p>";
+      }
+    break;
+  }
+}
+
 $schedules = selectSchedule();
 include "view-schedule.php";
 include "view-footer.php";
