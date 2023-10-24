051d97a4faff2264ef477cf13738083ae723025b
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-24 10:48:52 -0500

    Update view-sportid-input-list.php

--- view-sportid-input-list.php
+++ view-sportid-input-list.php
@@ -1,9 +1,9 @@
-<select class="form-select" id="sid" name="sid">
+<select class="form-select" id="schIDD" name="schIDD">
   <?php
 while ($sportItem = $schIDList->fetch_assoc()) {
   ?>
   <option value="<?php echo $sportItem['sport_id']; ?>"><?php echo $sportItem['sport_name']; ?></option>
   <?php
-}
+}https://github.com/ErnTern/hw3/blob/hw4/view-sportid-input-list.php
   ?>
 </select>
