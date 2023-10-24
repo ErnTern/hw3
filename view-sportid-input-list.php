7239db834018f66e2a89ae5f7e3c3adc60fe3a12
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-10-24 11:37:01 -0500

    Update view-sportid-input-list.php

--- view-sportid-input-list.php
+++ view-sportid-input-list.php
@@ -2,7 +2,7 @@
   <?php
 while ($sportItem = $schIDList->fetch_assoc()) {
   ?>
-  <option value="<?php echo $sportItem['sport_id']; ?>"><?php echo $sportItem['sport_name']; ?></option>
+  <option value="<?php echo $sportItem['sport_id']; ?>"><?php echo $sportItem['sport_id']; ?></option>
   <?php
 }https://github.com/ErnTern/hw3/blob/hw4/view-sportid-input-list.php
   ?>
