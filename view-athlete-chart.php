09ecc209e70ea1c3a375839cfcedd36fca42bb68
Aaron Tran <98052446+ErnTern@users.noreply.github.com>
2023-11-15 15:49:59 -0600

    Update view-athlete-chart.php

--- view-athlete-chart.php
+++ view-athlete-chart.php
@@ -130,26 +130,24 @@ $athlete = selectAthlete();
 <html>
   <head>
     <title>My first Chartist Tests</title>
-    <link rel="stylesheet"
-          href="bower_components/chartist/dist/chartist.min.css">
+    <link rel="stylesheet" href="bower_components/chartist/dist/chartist.min.css">
+    <script src="bower_components/chartist/dist/chartist.min.js"></script>
   </head>
   <body>
-    <!-- Site content goes here !-->
-    <script src="bower_components/chartist/dist/chartist.min.js"></script>
+    <!-- Site content goes here! -->
+
+    <div class="ct-chart ct-perfect-fourth"></div>
+
+    <script>
+      var data = {
+        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        series: [
+          [5, 2, 4, 2, 0]
+        ]
+      };
+
+      new Chartist.Line('.ct-chart', data);
+    </script>
   </body>
 </html>
-<div class="ct-chart ct-perfect-fourth"></div>
-var data = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5, 2, 4, 2, 0]
-  ]
-};
-
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line('.ct-chart', data);
 
